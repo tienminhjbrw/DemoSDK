fa340bcd82074234375307758673f0e3c14371ea
Tien Minh <leminhtien9cvl@gmail.com>
2024-03-19 16:32:37 +0700

    Tiến - DemoSDK: Khởi tạo project

---
+++ app/src/main/java/com/example/demosdk/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.demosdk;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainActivity extends AppCompatActivity {
+    public LoginButton loginButton;
+    private static final String GAMINGPROFILE = "gaming_profile";
+    private static final String GAMINGUSERPICTURE = "gaming_user_picture";
+    private static final String OPENID = "openid";
+    private static final String EMAIL = "email";
+    private static final String PUBLICPROFILE = "public_profile";
+    public CallbackManager callbackManager = CallbackManager.Factory.create();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions(GAMINGPROFILE);
+
+        getKeyhash(getApplicationContext());
+
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast.makeText(getApplicationContext(), "Login succesfully" + loginResult.getAccessToken().getToken(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(getApplicationContext(), "Login cancel", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+            }
+        });
+    }
+
+    public static void getKeyhash(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    context.getApplicationContext().getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            for (android.content.pm.Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.e("KEY HASH:",
+                        Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+}
